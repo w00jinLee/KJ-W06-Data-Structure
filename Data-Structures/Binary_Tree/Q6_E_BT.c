851a7e3f86467941d6f33aab882d6759cd961cbb
w00jinLee <leewoojin001121@gmail.com>
2026-04-09 20:07:30 +0900

    [WEEK6] 구현 - Q6_E_BT.c

--- Data-Structures/Binary_Tree/Q6_E_BT.c
+++ Data-Structures/Binary_Tree/Q6_E_BT.c
@@ -106,6 +106,17 @@ int main()
 void printSmallerValues(BTNode *node, int m)
 {
 	/* 여기에 코드를 작성하세요 */
+    if(node == NULL){
+        return;
+    }
+
+    if(node-> item < m){
+        printf("%d" ,node->item);
+    }
+
+    printSmallerValues(node->left, m);
+    printSmallerValues(node->right, m);
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////
